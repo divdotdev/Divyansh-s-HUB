3381431357072e34d4afa4a2232956853ce41b44
Divyansh Agarwal <divyansh29@Divyanshs-MacBook-Air-2.local>
2025-12-01 09:18:50 +0530

    xyz

---
+++ new.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"*";
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=4;
+    int i;
+    for(i=0; i<=4; i++)
+    {
+        cout<<"*";
+    }
+    return 0;
+}
